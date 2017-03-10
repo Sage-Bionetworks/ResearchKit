155524afb00e4e741499877e4c515dc8b5b2974f
Shannon Young <shannon.young@sagebase.org>
2017-03-10 15:38:22 -0800

    Validate that the developer has added the required authorizations.

--- ResearchKit/Common/ORKTaskViewController.m
+++ ResearchKit/Common/ORKTaskViewController.m
@@ -45,6 +45,7 @@
 #import "ORKFormStep.h"
 #import "ORKInstructionStep.h"
 #import "ORKOrderedTask.h"
+#import "ORKPermissionsValidator.h"
 #import "ORKQuestionStep.h"
 #import "ORKResult_Private.h"
 #import "ORKReviewStep_Internal.h"
@@ -471,6 +472,13 @@ static NSString *const _ChildNavigationControllerRestorationKey = @"childNavigat
     }
     
     ORKPermissionMask permissions = [self desiredPermissions];
+    
+    // Validate permissions
+    NSError *error;
+    if (![ORKPermissionsValidator validatePermissions:permissions writeTypes:writeTypes readTypes:readTypes error:&error]) {
+        ORK_Log_Error(@"%@: %@", [error localizedFailureReason], [error localizedDescription]);
+        NSAssert(NO, [error description]);
+    }
     
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     
